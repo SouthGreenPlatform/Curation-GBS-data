82637eda6c06ec6efe49f09fa0df1a30de7a9f5a
Luc Baudouin <luc.baudouin@cirad.fr>
2017-02-14 16:06:29 +0100

    fichiers d'entrée et ajustements mineurs

--- tassel_to_J_parameters.R
+++ tassel_to_J_parameters.R
@@ -6,7 +6,7 @@
 
 # The 240+2 individuals of the 1 to 6 plate set
 #source("D:/Mes donnees/sequencage/2017/article sequence/Tassel_to_J/TtJGIT/tassel_to_J_parameters_1to6.R")
-source("first ROUND.R")
+source("first_round.R")
 #source("D:/Mes donnees/sequencage/2017/article sequence/Tassel_to_J/TtJGIT/tassel_to_J_parameters_1to6.R")
 # Adding more large scaffolds, keeping the same number of individuals
 #source("D:/Mes donnees/sequencage/2017/article sequence/Tassel_to_J/TtJGIT/tassel_to_J_parameters_1to6_PR_07.R")
